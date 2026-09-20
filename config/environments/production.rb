b0e9d4024aa6abb702f3b5ff768c2ba0965f7956
Youki Z <pqrsdeumai@gmail.com>
2026-09-20 14:09:02 +0900

    memory_storeのコメントを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,10 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Replace the default in-process memory cache store with a durable alternative.
+  # デフォルトのプロセス内メモリキャッシュストアを使用します(再起動すると消える非永続的なストアです)。
   config.cache_store = :memory_store
 
-  # Replace the default in-process and non-durable queuing backend for Active Job.
+  # Active Jobをプロセス内のバックグラウンドスレッドで非同期実行する
   config.active_job.queue_adapter = :async
 
   # Ignore bad email addresses and do not raise email delivery errors.
