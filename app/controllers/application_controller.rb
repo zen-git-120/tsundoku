e70059e1331721687b514d2c475f21e2f835f3df
Youki Z <pqrsdeumai@gmail.com>
2026-09-21 20:12:45 +0900

    feat: ログイン成功後の遷移先をbooks_pathに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    books_path
+  end
 end
