9d8cd6279442928952bef6456232dc50edff1ae1
Youki Z <pqrsdeumai@gmail.com>
2026-09-22 21:54:47 +0900

    feat:本の登録に向けてモデルの入力チェックとルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "pages#top"
-  resources :books, only: [ :index ]
+  resources :books, only: [ :index, :new, :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
